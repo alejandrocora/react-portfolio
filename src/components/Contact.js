456204e3de1a37cb8364b3d7d671da1d6e74a89d
alejandrocora <alzapicolara@gmail.com>
2023-08-04 13:20:58 +0200

    added tick animation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,28 @@
 import { AiFillLinkedin, AiOutlinePhone, AiOutlineMail, AiOutlineCopy, AiOutlineWhatsApp } from "react-icons/ai";
+import { TiTickOutline } from "react-icons/ti";
 import { Trans } from 'react-i18next';
+import { useState, useEffect } from 'react';
 
 function Contact() {
-  function copyToClipboard(t, a) {
-    navigator.clipboard.writeText(t);
-    alert(a);
+
+  //<AiOutlineCopy className="copyLogo" onClick={() => copyToClipboard('+34 663 300 476', 0)} />, <AiOutlineCopy className="copyLogo" onClick={() => copyToClipboard('alzapicolara@gmail.com', 1)}/>
+
+  const [copyData, setCopyData] = useState([false, false]);
+
+  function copyToClipboard(text, index) {
+    navigator.clipboard.writeText(text);
+    setCopyData((prevState) => {
+      const newState = [...prevState]; // workaround shallow comparison
+      newState[index] = true;
+      return newState;
+    });
+    setTimeout(() => {
+      setCopyData((prevState) => {
+        const newState = [...prevState];
+        newState[index] = false;
+        return newState;
+      });
+    }, 750)
   }
 
   return (
@@ -14,11 +32,29 @@ function Contact() {
       <div id="contact" className="contact-container">
           <div>
             <AiOutlinePhone className="logo" />
-            <p><a href="tel:+34663300476">+34 663 300 476</a> <AiOutlineCopy className="copyLogo" onClick={() => copyToClipboard('+34 663 300 476', 'Número copiado.')} /><a target="_blank" href="https://wa.me/+34663300476"><AiOutlineWhatsApp className="copyLogo" /></a></p>
+            <p>
+              <a href="tel:+34663300476">+34 663 300 476</a>
+              {copyData[0] ? (
+                  <TiTickOutline />
+                ) : (
+                  <AiOutlineCopy className="copyLogo" onClick={() => copyToClipboard('+34 663 300 476', 0)} />
+                )
+              }
+              <a target="_blank" href="https://wa.me/+34663300476">
+              <AiOutlineWhatsApp className="copyLogo" /></a>
+            </p>
           </div>
           <div>
             <AiOutlineMail className="logo" />
-            <p><a target="_blank" href="mailto:alzapicolara@gmail.com">alzapicolara@gmail.com</a> <AiOutlineCopy className="copyLogo" onClick={() => copyToClipboard('alzapicolara@gmail.com', 'Correo copiado.')}/></p>
+            <p>
+              <a target="_blank" href="mailto:alzapicolara@gmail.com">alzapicolara@gmail.com</a>
+              {copyData[1] ? (
+                  <TiTickOutline />
+                ) : (
+                  <AiOutlineCopy className="copyLogo" onClick={() => copyToClipboard('alzapicolara@gmail.com', 1)} />
+                )
+              }
+            </p>
           </div>
           <div>
             <AiFillLinkedin className="logo" />
