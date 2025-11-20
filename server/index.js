534510f009ceb23839d1c03de7229cc866d7aee6
alejandrocora <alzapicolara@gmail.com>
2025-11-20 02:08:42 +0100

    last fixes

--- server/index.js
+++ server/index.js
@@ -2,15 +2,14 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static("public"));
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.static(buildPath));
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
 });
 
-const PORT = 3000;
-
-app.listen(PORT, 'localhost', () => {
-  console.log(`Server open on ${PORT}.`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
